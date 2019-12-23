d85a828ad759f346c2d95bb6cfd9bb71e6488a05
thrugsis <realtorgeorgeb@gmail.com>
2019-12-23 13:57:52 -0500

    intial commit

---
+++ serve-article/index.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        headers:{
+            'Access-Control-Allow-Origin': '*'
+        },
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
